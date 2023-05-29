1cb60661d912efeb1689de2473bff9597818564b
lilGITnamedME-T <121561729+lilGITnamedME-T@users.noreply.github.com>
2023-05-29 19:28:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX= 0;
+noseY= 0;
+
+function preload(){
+    mustache_filter= loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
+function setup(){
+    canvas= createCanvas(300, 300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(mustache_filter, noseX -40 , noseY -10 , 80, 60);
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+function modelLoaded(){
+    console.log('PoseNet Is Intialized');
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX= results[0].pose.nose.x;
+        noseY= results[0].pose.nose.y
+        console.log("nose x = "+ noseX);
+        console.log("nose y = "+ noseY);
+    }
+}
